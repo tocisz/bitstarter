f8237aeba3dcbbd3dbde2a26077ebdb086010ef5
Tomasz Cichocki <cichymail@gmail.com>
2013-07-10 21:31:20 +0200

    read from disc

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var idxBuf = fs.readFileSync('index.html')
+  var idxHtml = idxBuf.toString()
+  response.send(idxHtml);
 });
 
 var port = process.env.PORT || 5000;
